2e28f71809756a103af285e7001649fe0d1ee6e5
Lukecin <dluqueru@gmail.com>
2022-04-07 16:27:37 +0200

    create obstacle class and move one obstacle

--- js/game.js
+++ js/game.js
@@ -9,6 +9,18 @@ class Game {
         this.player = new Player();
         this.player.domElement = this.create('player');
         this.draw(this.player);
+
+        this.obstacle = new Obstacle();
+        this.obstacle.domElement = this.create('obstacle');
+        this.draw(this.obstacle);
+        
+        setInterval(() => {
+            this.draw(this.obstacle);
+            this.obstacle.moveDown();
+        }, 50)
+        
+        
+        
     }
 
     movePlayer(direction){
@@ -24,7 +36,7 @@ class Game {
 
 class Player {
     constructor(){
-        this.positionX = 40;
+        this.positionX = 45;
         this.positionY = 0;
         this.domElement = 0;
     }
@@ -37,5 +49,17 @@ class Player {
     moveRigth(){
         this.positionX++;
         console.log(`moving right... ${this.positionX}`)
+    }
+}
+
+class Obstacle {
+    constructor(){
+        this.positionX = 45;
+        this.positionY = 90;
+        this.domElement = null;
+    }
+
+    moveDown(){
+        this.positionY--;
     }
 }
